1779854789bf3fe4e9f950ced7f5cf0ed916d3ad
Ahmad Sharif <ahmadshorif@gmail.com>
2018-02-12 03:46:01 +0600

    Frontend

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,22 @@
 function startGame() {
-
-	console.log("startGame");
-
 	var userBlock = document.getElementById('userInfoSection');
 	userBlock.style.display = "none";
+
+	document.getElementById('questionBlock').style.display = 'block';
+}
+
+function goToNextQuestion () {
+	var radioButtons = document.getElementsByName("answerA");
+	
+
+	for (var i = 0; i < radioButtons.length; i++) {
+		if (radioButtons[i].checked) {
+			var selectValue =  radioButtons[i].value;
+			console.log(selectValue);
+			var elementId = event.srcElement.id
+			  console.log(elementId);
+			  $('#question0').hide();
+		}
+	}
+	
 }
