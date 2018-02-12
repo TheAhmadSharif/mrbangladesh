777af828dee98593f74dd3c7fc95f58f7acf99d3
Ahmad Sharif <ahmadshorif@gmail.com>
2018-02-12 22:29:36 +0600

    Complete Javascript section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,35 @@
 function startGame() {
 	var userBlock = document.getElementById('userInfoSection');
 	userBlock.style.display = "none";
-
 	document.getElementById('questionBlock').style.display = 'block';
-}
 
+	showQuestion()
+	
+}
+var number = 0;
 function goToNextQuestion () {
 	var radioButtons = document.getElementsByName("answerA");
 	
-
 	for (var i = 0; i < radioButtons.length; i++) {
 		if (radioButtons[i].checked) {
 			var selectValue =  radioButtons[i].value;
-			console.log(selectValue);
 			var elementId = event.srcElement.id
-			  console.log(elementId);
-			  $('#question0').hide();
 		}
+	}
+
+	if (number < 3 ) {
+		$(radioButtons).closest('.question-block').hide();
+		console.log(number);
+		showQuestion ();
+	}
+}
+
+
+function showQuestion () {
+	var elem = 'question' + number;
+	if (number < 3) {
+		document.getElementById(elem).style.display = 'block';
+		number = number + 1;
 	}
 	
 }
